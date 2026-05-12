4e08de18c684355d52de57e2970c80c5cf33b046
ZakDav815 <673804@eastridingcollege.ac.uk>
2026-05-12 22:53:10 +0100

    Tried to add a platform
    Wanted to have a moving platform so found a video tutorial. Got it mostly done but said platform doesn't move. Would try and figure it out but not at this time currently(23:00).

---
+++ Assets/Scripts/PlatformMove.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour
+{
+    public Transform Player;
+    public Transform parentObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Player == null) Player = other.transform; // 
+            Player.parent = parentObject; // set parent to parent object
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Player == null) Player = other.transform;
+            Player.parent = null; // set player back to worldspace when leave platform.
+        }
+    }
+}
